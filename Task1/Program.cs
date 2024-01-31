d9d166c67eb599ca53d8151ad99fc2dc6e020d7e
andreeff_andrey <andevise@gmail.com>
2024-01-31 23:43:12 +0300

    Домашнее задание_урок 7_ рекурсия

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+{
+    Console.WriteLine("Введите целое число M:");
+    int M = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите целое число N:");
+    int N = int.Parse(Console.ReadLine());
+
+    int min, max;
+
+    if (M < N)
+    {
+        min = M;
+        max = N;
+    }
+    else
+    {
+        min = N;
+        max = M;
+    }
+
+    PrintNumbers(min, max);
+}
+
+static void PrintNumbers(int m, int n)
+
+{
+    if (m > n)
+
+        return;
+
+    Console.Write(m);
+    if (m < n)
+    Console.Write(", ");
+
+    PrintNumbers(++m, n);
+}
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+
+        PrintArrayRevers(array, array.Length - 1);
+
+        Console.ReadLine();
+
+    }
+    static void PrintArrayRevers(int[] arr, int i)
+
+    {
+        if (i < 0)
+            return;
+
+        Console.Write(arr[i] + " ");
+
+        PrintArrayRevers(arr, i - 1);
+    }
+}
+
+
